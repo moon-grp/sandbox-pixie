9cf579e4fbaa80b4facd46c5e522b79dcce66ee5
moon-grp <olumidemm@gmail.com>
2019-11-17 14:36:42 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,140 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      light
+      elevate-on-scroll
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+        <span>PeeXoo</span>
+      </div>
+
+      <v-spacer></v-spacer>
+      <v-btn
+        class="custom-btn"
+        rounded
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2 ">I am a Photographer</span>
+      </v-btn>
+
+     <v-btn
+        class="custom-btn"
+        rounded
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Explore Photos</span>
+      
+      </v-btn>
+
+      <v-btn
+        class="custom-btn"
+        rounded
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Find Photographer</span>
+      
+      </v-btn>
+
+      <v-btn
+        class="custom-btn"
+        rounded
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Find my Photos</span>
+      
+      </v-btn>
+
+      <v-btn
+        class="custom-btn"
+        rounded
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Login or Register</span>
+      </v-btn>
+    </v-app-bar>
+   
+   <v-row>
+      <v-img
+      
+       :src="require('../assets/logo.svg')"
+      aspect-ratio="1"
+      class="grey lighten-2"
+      max-width="100%"
+      max-height="100vh"
+    ></v-img>
+   </v-row>
+
+   <v-row>
+      <v-toolbar>
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>Link 1</v-btn>
+        <v-btn text>Link 2</v-btn>
+        <v-btn text>Link 3</v-btn>
+      </v-toolbar-items>
+
+      <template v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn icon>
+          <v-icon>mdi-export-variant</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-delete-circle</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-plus-circle</v-icon>
+        </v-btn>
+      </template>
+    </v-toolbar>
+   </v-row>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'app',
+  name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<style >
+  .custom-btn::before {
+    color: transparent
+}
+
+.custom-btn:hover {
+    background-color:#FFB300;
 }
 </style>
